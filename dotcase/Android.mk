270d7a0a8663b4aa1dfa62ffc70ab55a6c1d06dd
invisiblek <dan.pasanen@gmail.com>
2014-05-15 17:30:32 -0500

    dotcase: add default proguard flags
    Change-Id: I77bd19f620d28736ae5be86ca5d8220be0c3de75

--- dotcase/Android.mk
+++ dotcase/Android.mk
@@ -10,6 +10,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := Dotcase
 LOCAL_CERTIFICATE := platform
 
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
 include $(BUILD_PACKAGE)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
