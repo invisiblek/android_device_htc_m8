2ef3b64fcd5c0e55d5a648e96bb7bebb39e9424a
invisiblek <dan.pasanen@gmail.com>
2014-06-07 23:46:53 -0500

    dotcase: add a torch option
    * long press to activate (or if torch is already on)
    * shows a stupid little light bulb icon that
    * you can either tap that stupid icon to turn the torch off
      or long press again
    
    Change-Id: I64062c03122928a6f565a539a4ac504adf05ff2c

--- dotcase/src/org/cyanogenmod/dotcase/Dotcase.java
+++ dotcase/src/org/cyanogenmod/dotcase/Dotcase.java
@@ -37,6 +37,7 @@ import android.view.View;
 import android.view.WindowManager;
 
 import com.android.internal.telephony.ITelephony;
+import com.android.internal.util.cm.TorchConstants;
 
 import java.lang.Math;
 import java.io.BufferedReader;
@@ -56,6 +57,7 @@ public class Dotcase extends Activity
     public static boolean ringing = false;
     public static int ringCounter = 0;
     public static String phoneNumber = "";
+    public static boolean torchStatus = false;
 
     public static boolean gmail = false;
     public static boolean hangouts = false;
@@ -74,6 +76,7 @@ public class Dotcase extends Activity
         mContext = this;
 
         filter.addAction(DotcaseConstants.ACTION_KILL_ACTIVITY);
+        filter.addAction(TorchConstants.ACTION_STATE_CHANGED);
         mContext.getApplicationContext().registerReceiver(receiver, filter);
 
         getWindow().addFlags(
@@ -195,6 +198,13 @@ public class Dotcase extends Activity
 
     class DotcaseGestureListener extends GestureDetector.SimpleOnGestureListener
     {
+
+        @Override
+        public void onLongPress(MotionEvent event) {
+            Intent i = new Intent(TorchConstants.ACTION_TOGGLE_STATE);
+            mContext.sendBroadcast(i);
+        }
+
         @Override
         public boolean onDoubleTap(MotionEvent event) {
             manager.goToSleep(SystemClock.uptimeMillis());
@@ -235,7 +245,16 @@ public class Dotcase extends Activity
         }
 
         @Override
-        public boolean onSingleTapConfirmed (MotionEvent e) {
+        public boolean onSingleTapUp (MotionEvent e) {
+            if (Dotcase.torchStatus) {
+                if (e.getX() >19 * DotcaseConstants.dotratio
+                        && e.getX() < 26 * DotcaseConstants.dotratio
+                        && e.getY() > 22 * DotcaseConstants.dotratio
+                        && e.getY() < 32 * DotcaseConstants.dotratio){
+                    Intent i = new Intent(TorchConstants.ACTION_TOGGLE_STATE);
+                    mContext.sendBroadcast(i);
+                }
+            }
             reset_timer = true;
             return true;
         }
@@ -251,6 +270,8 @@ public class Dotcase extends Activity
                 running = false;
                 finish();
                 overridePendingTransition(0, 0);
+            } else if (intent.getAction().equals(TorchConstants.ACTION_STATE_CHANGED)) {
+                torchStatus = intent.getIntExtra(TorchConstants.EXTRA_CURRENT_STATE, 0) != 0;
             }
         }
     };
